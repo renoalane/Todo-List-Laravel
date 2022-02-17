60f528c50e9ea26a6deaac7ee00862b2c3fc6960
renoalane <renopradino@gmail.com>
2022-02-18 00:14:51 +0700

    route todo and todos

--- routes/web.php
+++ routes/web.php
@@ -26,13 +26,18 @@ Route::get('/login', [LoginController::class, 'index'])->name('login')->middlewa
 Route::post('/login', [LoginController::class, 'autenticate'])->name('login.autenticate');
 Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
 
-// Todo
-Route::get('/todos', [TodoController::class, 'index'])->name('todo');
-Route::post('/todos', [TodoController::class, 'store'])->name('todo.store');
-Route::get('/{todo}/completed', [TodoController::class, 'completed'])->name('todo.completed');
-Route::get('/todos/{todo}', [TodoController::class, 'edit'])->name('todo.edit');
-Route::post('/todos/{todo}', [TodoController::class, 'update'])->name('todo.update');
-Route::delete('/todos/{todo}', [TodoController::class, 'destroy']);
 
-// TodoList
-Route::get('/todoslist', [TodoListController::class, 'findAll'])->name('todolist');
+Route::middleware('auth')->group(function () {
+
+    // Todo
+    Route::get('/todos', [TodoController::class, 'index'])->name('todo');
+    Route::post('/todos', [TodoController::class, 'store'])->name('todo.store');
+    Route::get('/{todo}/completed', [TodoController::class, 'completed'])->name('todo.completed');
+    Route::get('/todos/{todo}', [TodoController::class, 'edit'])->name('todo.edit');
+    Route::put('/todos/{todo}', [TodoController::class, 'update'])->name('todo.update');
+    Route::delete('/todos/{todo}', [TodoController::class, 'destroy']);
+
+    // TodoList
+    Route::get('/todolist', [TodoListController::class, 'findAll'])->name('todolist');
+    Route::delete('/todolist/{todo}', [TodoListController::class, 'destroy'])->name('todolist.destroy');
+});
